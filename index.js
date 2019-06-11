d350bc57ba586876d5156585d7af5f5cc3b9ac53
Connor Greenbaum <connor_greenbaum@MacBook-Pro.local>
2019-06-10 21:09:52 -0400

    intiialcommit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
@@ -7,12 +7,42 @@ function ask(questionText) {
   });
 }
 
-start();
+//The computer then guesses within the predetermined range
+let computerGuess = (max, min) => {
+  let guess = (min + max) / 2;
+  return Math.floor(guess);
+};
 
+start();
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
+  let playing = true;
+  let upperBound = 100
+  let lowerBound = 1
+  let currentGuess = computerGuess(upperBound, lowerBound);
+  console.log(
+    "Let's play a game where you (human) make up a number and I (computer) try to guess it."
+  );
+  //The user picks a secret number
+  let secretNumber = await ask(
+    "What is your secret number?\nI won't peek, I promise...\n"
+  );
+  console.log("You entered: " + secretNumber);
+  while (playing) {
+    let yesOrNo = await ask("is your number " + currentGuess + "? ");
+    if (yesOrNo.toLowerCase().includes("yes")) {
+      console.log("You're correct, that is my number!");
+      process.exit();
+    } else {
+      let highOrLow = await ask("Is your number higher or lower? ");
+      if (highOrLow.toLowerCase().includes("higher")) {
+        lowerBound = currentGuess;
+        currentGuess = computerGuess(upperBound, lowerBound);
+      } else if (highOrLow.toLowerCase().includes("lower")) {
+        upperBound = currentGuess;
+        currentGuess = computerGuess(upperBound, lowerBound);
+      } else {
+        console.log("Please type higher or lower");
+      }
+    }
+  }
 }
